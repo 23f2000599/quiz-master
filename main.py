016616e74e9af7daee388864618db44bc3794523
smriti-s18 <23f2000599@ds.study.iitm.ac.in>
2025-04-24 13:28:05 +0530

    changes

--- main.py
+++ main.py
@@ -15,6 +15,22 @@ configure_routes(app)
 with app.app_context():
     db.create_all()
     print("Database and tables created")
+    admin = User.query.filter_by(username='admin').first()
+    if not admin:
+        # Create admin user
+        admin_user = User(
+            username='admin',
+            email="admin@gmail.com",
+            password="admin123",
+            role="admin",
+            dob="2000-01-01",  
+        )
+        
+        # Add to database
+        db.session.add(admin_user)
+        db.session.commit()
+        print("Admin user created")
+
 
 if __name__== "__main__":
     app.run(debug=True)
